4755992bc88294bd297ededfc705f0646eaabcfc
Giorgi Zhorzholiani <jorjika@live.com>
2020-09-04 23:17:43 +0400

    documented IQueryBusAsync

--- src/Digify.Micro/Queries/QueryBus.cs
+++ src/Digify.Micro/Queries/QueryBus.cs
@@ -6,6 +6,15 @@ namespace Digify.Micro.Queries
 {
     public interface IQueryBusAsync
     {
+        /// <summary>
+        /// Find handler for specific query
+        /// </summary>
+        /// <typeparam name="TQuery">Query parameter type</typeparam>
+        /// <typeparam name="TResult">Query result type</typeparam>
+        /// <param name="query">Query object</param>
+        /// <returns>Returns result of query</returns>
+        /// <exception cref="ArgumentNullException">Thrown when query argument is null.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when handler is not found.</exception>
         Task<TResult> ExecuteAsync<TQuery, TResult>(TQuery query) where TQuery : IQuery;
     }
 
@@ -15,6 +24,7 @@ namespace Digify.Micro.Queries
 
         public QueryBusAsync(ILifetimeScope context) => this.context = context ?? throw new ArgumentNullException(nameof(context));
 
+     
         public Task<TResult> ExecuteAsync<TQuery, TResult>(TQuery query) where TQuery : IQuery
         {
             if (query == null)
