ade6114f3170e0351d252c7dda6c57c209b5013d
Giorgi Zhorzholiani <jorjika90@gmail.com>
2020-09-09 23:26:23 +0400

    command validator added

--- src/Digify.Micro/Commands/CommandBus.cs
+++ src/Digify.Micro/Commands/CommandBus.cs
@@ -12,11 +12,9 @@ namespace Digify.Micro.Commands
 
     public class CommandBusAsync : ICommandBusAsync
     {
-        private readonly IPipelineBehavior pipeline;
         private readonly ILifetimeScope context;
-        public CommandBusAsync(ILifetimeScope context, IPipelineBehavior pipeline)
+        public CommandBusAsync(ILifetimeScope context)
         {
-            this.pipeline = pipeline;
             this.context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
@@ -26,9 +24,12 @@ namespace Digify.Micro.Commands
                 throw new ArgumentNullException($"Command shouldn't be null");
 
             TResult result;
-            await pipeline.Handle<TCommand, TResult>(command);
+
             using (var scope = context.BeginLifetimeScope())
             {
+                var validationHandler = scope.ResolveOptional<ICommandValidationBehaviour<TCommand>>();
+                if (validationHandler != null) await validationHandler.Handle(command);
+
                 var handler = scope.Resolve<ICommandHandlerAsync<TCommand, TResult>>()
                     ?? throw new InvalidOperationException($"Handler not found for specified command");
 
@@ -45,6 +46,9 @@ namespace Digify.Micro.Commands
 
             using (var scope = context.BeginLifetimeScope())
             {
+                var validationHandler = scope.ResolveOptional<ICommandValidationBehaviour<TCommand>>();
+                if (validationHandler != null) await validationHandler.Handle(command);
+
                 var handler = scope.Resolve<ICommandHandlerAsync<TCommand>>()
                     ?? throw new InvalidOperationException($"Handler not found for specified command");
 
