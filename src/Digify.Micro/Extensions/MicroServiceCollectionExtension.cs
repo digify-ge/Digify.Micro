1c08d3f0284f5aae2261ff6110502eaa521c2989
Giorgi Zhorzholiani <jorjika@live.com>
2020-10-22 13:35:54 +0400

    fix: preloading assemblies for assembly scan to register handlers and required services

--- src/Digify.Micro/Extensions/MicroServiceCollectionExtension.cs
+++ src/Digify.Micro/Extensions/MicroServiceCollectionExtension.cs
@@ -4,6 +4,7 @@ using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -12,7 +13,9 @@ namespace Digify.Micro.Extensions
 {
     public static class MicroServiceCollectionExtension
     {
-        private static IEnumerable<Assembly> _assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(e => !e.IsDynamic);
+        private static IEnumerable<Assembly> _allAssemblies;
+        private static IEnumerable<Assembly> _assemblies;
+
 
         /// <summary>
         /// Adding Commands, Queries, Domains and Fluent Validation for CQRS.
@@ -21,6 +24,10 @@ namespace Digify.Micro.Extensions
         /// <returns></returns>
         public static IServiceCollection AddMicro(this IServiceCollection services, MicroSettings settings = default)
         {
+            ReloadAssemblyCollections();
+            PreLoadAssemblies();
+            ReloadAssemblyCollections();
+
             services.AddValidatorsFromAssemblies(_assemblies);
 
             MicroSettings microSettings = settings;
@@ -32,6 +39,7 @@ namespace Digify.Micro.Extensions
             services.AddRequestHandlers();
             return services;
         }
+
         private static IServiceCollection AddRequestHandlers(this IServiceCollection services)
         {
             services.AddTransient<IBusAsync, BusAsync>();
@@ -53,6 +61,7 @@ namespace Digify.Micro.Extensions
                 }
 
             }
+
             return services;
         }
 
@@ -60,7 +69,7 @@ namespace Digify.Micro.Extensions
         {
             services.AddTransient<ServiceScope>(p => p.GetService);
 
-            var exportedTypes = GetExportedTypes(typeof(IPipelineBehavior<,>), 
+            var exportedTypes = GetExportedTypes(typeof(IPipelineBehavior<,>),
                 typeof(IRequestPreProcessor<>),
                 typeof(IRequestPostProcessor<,>));
             foreach (var @type in exportedTypes)
@@ -81,6 +90,62 @@ namespace Digify.Micro.Extensions
           .Distinct()
           .ToList();
             return exportedTypes;
+        }
+
+        private static IEnumerable<Type> GetExportedTypes(Assembly assembly, Type[] type)
+        {
+            var exportedTypes = assembly.ExportedTypes
+          .Where(e => e.GetTypeInfo().ImplementedInterfaces.Any(x => x.IsGenericType
+          && type.Contains(x.GetGenericTypeDefinition())))
+          .Distinct()
+          .ToList();
+            return exportedTypes;
+        }
+
+        private static void PreLoadAssemblies()
+        {
+            var entry = Assembly.GetEntryAssembly();
+            var referencedAssemblies = entry.GetReferencedAssemblies();
+
+            var microAssembly = typeof(MicroServiceCollectionExtension).Assembly;
+            var microAssemblyName = microAssembly.GetName();
+
+            var loadedAssemblyNames = _allAssemblies.Select(a => a.GetName());
+            var assemblyFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.dll", SearchOption.AllDirectories);
+
+            var assemblyFilesAndNames = new List<dynamic>();
+            foreach (var file in assemblyFiles)
+            {
+                try
+                {
+                    var name = AssemblyName.GetAssemblyName(file);
+                    var p = file;
+                    assemblyFilesAndNames.Add(new { Name = name, Path = p });
+                }
+                catch
+                {
+                }
+            }
+
+            var assembliesToLoad = assemblyFilesAndNames.Where(f => !loadedAssemblyNames.Any(lf => AssemblyName.ReferenceMatchesDefinition(lf, f.Name))).ToList();
+
+            foreach (var assembly in assembliesToLoad)
+            {
+                try
+                {
+                    Assembly.LoadFrom(assembly.Path);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private static void ReloadAssemblyCollections()
+        {
+            _allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            _assemblies = _allAssemblies.Where(e => !e.IsDynamic);
         }
     }
 }
