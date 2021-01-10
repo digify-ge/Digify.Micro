834e8c087b13931965d40cf6cad038f9fb6f1fc4
Giorgi Zhorzholiani <jorjika90@gmail.com>
2021-01-10 17:00:22 +0400

    field log attribute added

---
+++ src/Digify.Micro/Attributes/FieldLogBehaviourAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Digify.Micro.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class FieldLogBehaviourAttribute : Attribute
+    {
+        public readonly FieldLogBehaviourTypeEnum Behaviuor;
+
+        public FieldLogBehaviourAttribute(FieldLogBehaviourTypeEnum behaviuor = FieldLogBehaviourTypeEnum.LogFieldValue)
+        {
+            Behaviuor = behaviuor;
+        }
+    }
+
+    public enum FieldLogBehaviourTypeEnum
+    {
+        LogFieldValue,
+        IgnoreFieldValue,
+        LogSensitiveField
+    }
+}
